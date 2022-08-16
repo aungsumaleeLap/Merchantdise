5046288a4a6242a8be3cb3fc3ca2625e9e961cb8
Aungsumalee <aungsumalee.lap@gmail.com>
2022-08-16 12:41:11 +0700

    first commit

---
+++ src/components/counter.jsx
@@ -0,0 +1,40 @@
+import div, { Component } from 'react';
+
+class Counter extends Component {
+    state = {
+        value: this.props.value
+    };
+
+    handleIncrement = product =>{    
+       // console.log(product);    
+        this.setState({ value: this.state.value +1 })
+    };
+
+    render() {      
+        console.log("props", this.props);  
+
+        return (
+            <div>
+                <h4> Counter {this.props.id}</h4>
+                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button 
+                    onClick={ () => this.handleIncrement({ id : 1})} 
+                    className="btn btn-secondary btn-sm">Increment
+                </button>            
+            </div>
+        );
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += this.state.value === 0 ? "warning" : "primary";
+        return classes;
+    }
+
+    formatCount(){
+        const {value: count} = this.state;
+        return count === 0 ? "Zero" : count;
+    }
+}
+ 
+export default Counter;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
+import Counters from './components/counters';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Counters />
   </React.StrictMode>
 );
 
